8bc5eb4fb6cbd3accb8086c961423155446a8b32
Manuel Venegas <mengasis@outlook.com>
2017-07-30 21:56:12 -0400

    Route adaptation

---
+++ data/gods.js
@@ -0,0 +1,12 @@
+import config from '../config'
+
+export default {
+	'gods': [
+		{
+			'id': 1,
+			'name': 'Zeus',
+			'description': 'Hijo de los titanes Cronos y Rea, y fue el dios supremo de los griegos, que vivía en el monte Olimpo al noreste de Grecia. Allí moraba con su esposa Hera y los otros diez dioses mayores.',
+			'image': `${config.baseUrl}:${config.serverPort}/image/zeus.jpg`
+		}
+	]
+}
