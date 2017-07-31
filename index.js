6aefdc6d9e93f52c92214e965d27346695a4f7c7
Manuel Venegas <mengasis@outlook.com>
2017-07-31 18:03:48 -0400

    Serving static content

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import router from './router'
 const app = express()
 
 app.use('/api', router)
+app.use(express.static('public'))
+
 
 app.listen(config.serverPort, () => {
 	console.log(`Escuchando en ${config.baseUrl}:${config.serverPort}`)
